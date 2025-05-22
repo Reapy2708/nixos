cde1fb07ffc4381b3c839f6a1f4e5d616d9edd7b
Reapy2708 <michelw278@gmail.com>
2025-05-22 18:40:43 +0200

    Add files via upload

---
+++ hosts/overclockulus/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, inputs, lib, ... }:
+
+{
+  home.username = "sinan";
+  home.homeDirectory = "/home/sinan";
+
+  home.sessionVariables = {
+    TERMINAL = "kitty";
+  };
+  
+  imports = [
+    ../../modules/home-manager
+  ];
+
+  gaming.enable = true;
+  crypto.enable = false;
+  zsh.enable = true;
+  hyprland.enable = false;
+  #home.packages = with pkgs; [];
+
+  home.stateVersion = "24.11";
+  programs.home-manager.enable = true;
+}
